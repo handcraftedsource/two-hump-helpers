7e1f42ab6329657c6f2390d1852d69d05c4eb003
Handcrafted <f-fix@web.de>
2023-05-08 23:45:47 +0200

    Add a skill that performs a text generation task.

---
+++ src/TwoHumpHelpers/Linq/EnumerableExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel.Connectors.AI.OpenAI.Tokenizers;
+using Microsoft.SemanticKernel.Memory;
+
+namespace TwoHumpHelpers.Linq;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<TSource> TakeWhileAggregate<TSource, TAccumulate>(
+        this IEnumerable<TSource> source,
+        TAccumulate seed,
+        Func<TAccumulate, TSource, TAccumulate> func,
+        Func<TAccumulate, bool> predicate
+    ) {
+        TAccumulate accumulator = seed;
+        foreach (TSource item in source) {
+            accumulator = func(accumulator, item);
+            if (predicate(accumulator)) {
+                yield return item;
+            }
+            else {
+                yield break;
+            }
+        }
+    }
+
+    public static IEnumerable<MemoryQueryResult> TakeMaxTokens(this IEnumerable<MemoryQueryResult> memories, int maxTokens)
+    {
+        var inputMemories = memories
+            .TakeWhileAggregate(0, (total, current) => total + GPT3Tokenizer.Encode(current.Metadata.Text).Count, total => total < maxTokens);
+
+        return inputMemories;
+    }
+}
---
+++ src/TwoHumpHelpers/Skills/TextGenerationSkill.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.SemanticFunctions.Partitioning;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace TwoHumpHelpers.Skills;
+
+/// <summary>
+/// <para>Semantic skill that enables text generation.</para>
+/// </summary>
+/// <example>
+/// <code>
+/// var context = kernel.CreateNewContext();
+/// 
+/// context["input"] = "your context information";
+/// context["task"] = "the task to perform";
+/// 
+/// var skill = new TextGenerationSkill(kernel);
+/// kernel.ImportSkill(skill);
+/// 
+/// var result = await skill.SimpleTextGenerationTaskAsync(input, context);
+/// </code>
+/// </example>
+public class TextGenerationSkill
+{
+    /// <summary>
+    /// The max tokens to process in a single semantic function call.
+    /// </summary>
+    public const int SimpleTextGenerationTaskMaxContextTokens = 3097;
+
+    private readonly ISKFunction _simpleTextGenerationTaskFunction;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TextGenerationSkill"/> class.
+    /// </summary>
+    /// <param name="kernel">Kernel instance</param>
+    /// <param name="temperature">Temperature parameter passed to LLM. A value between 0 and 1.0. A higher temperature leads to more varied and creative outputs, while a lower temperature results in more predictable responses.</param>
+    public TextGenerationSkill(IKernel kernel, double temperature = 0.7)
+    {
+        _simpleTextGenerationTaskFunction = kernel.CreateSemanticFunction(
+            Prompts.SimpleTextGenerationTask,
+            skillName: nameof(TextGenerationSkill),
+            description: "Generates text based on a task and a given context.",
+            maxTokens: 1000,
+            temperature: temperature,
+            topP: 0.5);
+    }
+
+    /// <summary>
+    /// Generates text based on a task and a given context.
+    /// </summary>
+    /// <param name="input">A context for the task to perform. Only <see cref="SimpleTextGenerationTaskMaxContextTokens"/> tokens are used.</param>
+    /// <param name="context">The SKContext for function execution.</param>
+    [SKFunction("Generates text based on a task and a given context.")]
+    [SKFunctionName("SimpleTextGenerationTask")]
+    [SKFunctionInput(Description = "The context to perform the task with.")]
+    [SKFunctionContextParameter(Name = "task", Description = "The task that describes what text should be generated.")]
+    public Task<SKContext> SimpleTextGenerationTaskAsync(string input, SKContext context)
+    {
+        // TODO - Next NuGet version: renamed to TextChunker
+        var lines = SemanticTextPartitioner.SplitPlainTextLines(input, SimpleTextGenerationTaskMaxContextTokens);
+        var paragraphs = SemanticTextPartitioner.SplitPlainTextParagraphs(lines, SimpleTextGenerationTaskMaxContextTokens);
+
+        return _simpleTextGenerationTaskFunction
+            .AggregatePartitionedResultsAsync(paragraphs, context);
+    }
+
+    private static class Prompts
+    {
+        public const string SimpleTextGenerationTask = @"This is your context:
+---
+{{$input}}
+---
+
+This is your task:
+{{$task}}
+
+Answer:
+";
+    }
+}
