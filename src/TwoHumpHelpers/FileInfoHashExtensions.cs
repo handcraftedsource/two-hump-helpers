39a4aa01197e74cecd468f48c832a802405cc81b
Handcrafted <f-fix@web.de>
2023-05-08 23:35:46 +0200

    Add an extension method to create a md5 hash from a FileInfo.

---
+++ src/TwoHumpHelpers/FileInfoHashExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace TwoHumpHelpers;
+
+public static class FileInfoHashExtensions
+{
+    public static string CreateMd5Hash(this FileInfo file)
+    {
+        using var md5 = MD5.Create();
+        using var fileStream = file.Open(FileMode.Open, FileAccess.Read);
+        var hashValue = md5.ComputeHash(fileStream);
+        var result = BitConverter.ToString(hashValue).Replace("-", "").ToLowerInvariant();
+        return result;
+    }
+
+    public static string CreateMd5Hash(this string filePath)
+    {
+        var fileInfo = new FileInfo(filePath);
+        return fileInfo.CreateMd5Hash();
+    }
+}
